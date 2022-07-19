750555e6145d47ac17fae9910e63fe0c8b62e9eb
Nikita <nicita.zinkevitch@yandex.ru>
2022-07-19 14:13:47 +0300

    Add pills and login

---
+++ backend/views/layouts/_sidebar.php
@@ -0,0 +1,16 @@
+<?php echo \yii\bootstrap4\Nav::widget([
+        'options' => [
+                'class' => 'd-flex flex-column nav-pills'
+        ],
+        'items' => [
+            [
+                'label' => 'Dashboard',
+                'url' => ['/site/index']
+            ],
+            [
+                'label' => 'Videos',
+                'url' => ['/video/index']
+            ]
+        ]
+]);
+
